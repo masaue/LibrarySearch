28f6e1d90d2cedc0588f27d7542ff990066ebbb1
masaue <masayuki.uegaki@gmail.com>
2022-10-12 21:30:14 +0900

    add test about SafeAreaView backgroundColor

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,20 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from 'src/App';
-
+import {SafeAreaView} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+import App from 'src/App';
+
 it('renders correctly', () => {
-  renderer.create(<App />);
+  expect(_safeAreaViewColor()).toBe(Colors.lighter);
 });
+
+function _safeAreaViewColor() {
+  const app = renderer.create(<App />);
+  const safeAreaView = app.root.findByType(SafeAreaView);
+  return safeAreaView.props.style.backgroundColor;
+}
