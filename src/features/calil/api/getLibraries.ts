cea496bc5822686201d0b30a8435ec290bf09199
masaue <masayuki.uegaki@gmail.com>
2022-10-22 10:40:59 +0900

    fix getLibraries() response to json

--- src/features/calil/api/getLibraries.ts
+++ src/features/calil/api/getLibraries.ts
@@ -5,6 +5,8 @@ export const getLibraries = () => {
   return axios.get('/library', {
     params: {
       app_key: CALIL_API_KEY,
+      callback: '',
+      format: 'json',
       pref: '鳥取県',
     },
   });
