927e421fd7dd050d8a1280ce9a8520280bcb252a
masaue <masayuki.uegaki@gmail.com>
2022-10-22 23:23:42 +0900

    add TextInput for prefecture

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,20 @@
  */
 
 import React from 'react';
-import {Button, SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  TextInput,
+  useColorScheme,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import {getLibraries} from 'src/features/calil/api/getLibraries';
 
 const App = () => {
+  const [prefecture, setPrefecture] = React.useState('');
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -27,11 +35,17 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+      <TextInput
+        onChangeText={setPrefecture}
+        value={prefecture}
+        placeholder="都道府県"
+      />
       <Button
         color={Colors.blue}
         onPress={async () => {
-          const libraries = await getLibraries();
+          const libraries = await getLibraries(prefecture);
           console.log(libraries);
+          console.log(prefecture);
         }}
         title="call calil library"
       />
--- src/features/calil/api/getLibraries.ts
+++ src/features/calil/api/getLibraries.ts
@@ -2,13 +2,13 @@ import {CALIL_API_KEY} from '@env';
 import {axios} from 'src/lib/axios';
 import {Library} from 'src/features/calil/types/library';
 
-export const getLibraries = (): Promise<Library[]> => {
+export const getLibraries = (pref: string): Promise<Library[]> => {
   return axios.get('/library', {
     params: {
       app_key: CALIL_API_KEY,
       callback: '',
       format: 'json',
-      pref: '鳥取県',
+      pref,
     },
   });
 };
